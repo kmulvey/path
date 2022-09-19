bd3ce4989ca5c38166b95c6f36cbee971ba43cd1
Kevin Mulvey <kmulvey@linux.com>
2022-09-19 12:45:48 -0600

    add back filters

---
+++ filter.go
@@ -0,0 +1,60 @@
+package path
+
+import (
+	"io/fs"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/kmulvey/goutils"
+)
+
+// FilterFilesByDateRange removes files from the slice if they are not within the given date range.
+func FilterFilesByDateRange(files []Entry, beginTime, endTime time.Time) ([]Entry, error) {
+	for i := len(files) - 1; i >= 0; i-- {
+		if files[i].FileInfo.ModTime().Before(beginTime) || files[i].FileInfo.ModTime().After(endTime) {
+			files = goutils.RemoveElementFromArray(files, i)
+		}
+	}
+	return files, nil
+}
+
+// FilterFilesBySkipMap removes files from the map that are also in the skipMap.
+func FilterFilesBySkipMap(files []Entry, skipMap map[string]struct{}) []Entry {
+	for i := len(files) - 1; i >= 0; i-- {
+		if _, has := skipMap[files[i].AbsolutePath]; has {
+			files = goutils.RemoveElementFromArray(files, i)
+		}
+	}
+	return files
+}
+
+// FilterFilesByRegex removes files from the slice if they do not match the regex.
+func FilterFilesByRegex(files []Entry, filterRegex *regexp.Regexp) []Entry {
+	for i := len(files) - 1; i >= 0; i-- {
+		if !filterRegex.MatchString(strings.ToLower(files[i].AbsolutePath)) {
+			files = goutils.RemoveElementFromArray(files, i)
+		}
+	}
+	return files
+}
+
+// FilterFilesByPerms removes files from the slice if they are not in the given range.
+func FilterFilesByPerms(files []Entry, min, max uint32) ([]Entry, error) {
+	for i := len(files) - 1; i >= 0; i-- {
+		if files[i].FileInfo.Mode() < fs.FileMode(min) || files[i].FileInfo.Mode() > fs.FileMode(max) {
+			files = goutils.RemoveElementFromArray(files, i)
+		}
+	}
+	return files, nil
+}
+
+// FilterFilesBySize removes files from the slice if they are not in the given range.
+func FilterFilesBySize(files []Entry, min, max int64) ([]Entry, error) {
+	for i := len(files) - 1; i >= 0; i-- {
+		if files[i].FileInfo.Size() < min || files[i].FileInfo.Size() > max {
+			files = goutils.RemoveElementFromArray(files, i)
+		}
+	}
+	return files, nil
+}
