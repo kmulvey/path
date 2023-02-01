a8cec421e34ecf5a227d35a391ee4d816cc8e6da
Kevin Mulvey <kmulvey@linux.com>
2023-02-01 10:13:52 -0700

    test Contains()
    Signed-off-by: Kevin Mulvey <kmulvey@linux.com>

--- entry_test.go
+++ entry_test.go
@@ -1,6 +1,7 @@
 package path
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -42,4 +43,27 @@ func TestCrazyFileName(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(files))
 	assert.Equal(t, "./testdata/ogCGs91VSA5FBjJdgE8eeLSngbebPXyDCICZ7I~tplv-f5insbecw7-1 720 720.jpg", unglobbedPath)
+}
+
+func TestPopulateChildren(t *testing.T) {
+	t.Parallel()
+
+	var entry = Entry{AbsolutePath: "sdgwgwg/gwegtgh"}
+	assert.Error(t, entry.populateChildren(1))
+}
+func TestContains(t *testing.T) {
+	t.Parallel()
+
+	var entries, err = List("./testdata/")
+	assert.NoError(t, err)
+
+	var mp3Path string
+
+	for _, name := range entries {
+		if strings.Contains(name.AbsolutePath, ".mp3") {
+			mp3Path = name.AbsolutePath
+		}
+	}
+
+	assert.True(t, Contains(entries, mp3Path))
 }
