8d4e79ad5c9d47015190435d8963790ec9c7677e
Kevin Mulvey <kmulvey@linux.com>
2023-02-10 09:59:59 -0700

    helper tests
    Signed-off-by: Kevin Mulvey <kmulvey@linux.com>

--- helpers_test.go
+++ helpers_test.go
@@ -1,20 +1,48 @@
 package path
 
-/*
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 func TestContains(t *testing.T) {
 	t.Parallel()
 
-	var entries, err = List("./testdata/", 2)
+	var entry, err = NewEntry("./testdata/", 3)
 	assert.NoError(t, err)
+
+	files, err := entry.Flatten()
+	assert.NoError(t, err)
+	assert.Equal(t, 8, len(files))
 
 	var mp3Path string
 
-	for _, name := range entries {
+	for _, name := range files {
 		if strings.Contains(name.AbsolutePath, ".mp3") {
 			mp3Path = name.AbsolutePath
 		}
 	}
 
-	assert.True(t, Contains(entries, mp3Path))
+	assert.True(t, Contains(files, mp3Path))
+
+	entry, err = NewEntry("./helpers_test.go", 0)
+	assert.NoError(t, err)
+	assert.False(t, Contains(files, entry.AbsolutePath))
 }
-*/
+
+func TestOnlyNames(t *testing.T) {
+	t.Parallel()
+
+	var entry, err = NewEntry("./testdata/", 3)
+	assert.NoError(t, err)
+
+	files, err := entry.Flatten()
+	assert.NoError(t, err)
+	assert.Equal(t, 8, len(files))
+
+	for _, name := range OnlyNames(files) {
+		assert.True(t, strings.HasPrefix(name, "/"))
+	}
+}
