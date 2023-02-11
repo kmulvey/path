481c30eff4ee87b8726d666de117a8f4645049ee
Kevin Mulvey <kmulvey@linux.com>
2023-02-11 09:10:56 -0700

    test error
    Signed-off-by: Kevin Mulvey <kmulvey@linux.com>

--- list_test.go
+++ list_test.go
@@ -12,4 +12,8 @@ func TestList(t *testing.T) {
 	var files, err = List("./testdata/", 3)
 	assert.NoError(t, err)
 	assert.Equal(t, 8, len(files))
+
+	files, err = List("./notexist/", 3)
+	assert.Error(t, err)
+	assert.Equal(t, 0, len(files))
 }
